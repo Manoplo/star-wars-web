41ce244b7eb0ca91b8708aaf429231251662d2bc
Manoplo <manueluismartin@gmail.com>
2021-12-03 21:49:11 +0100

    Terminado

---
+++ js/saber.js
@@ -0,0 +1,9 @@
+
+const saber = document.getElementById('saber');
+const audio = document.getElementById('saberSound');
+
+
+function playSound(sound){
+    const mySound = document.getElementById(sound);
+    mySound.play();
+}
